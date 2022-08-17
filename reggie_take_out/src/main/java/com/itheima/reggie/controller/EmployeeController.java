b51e8fb08c85c831040756bbb68e0e51473bcdee
吴祖恩 <19302016001@fudan.edu.cn>
2022-08-17 21:08:06 +0800

    新增员工

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -53,5 +54,24 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request){
         request.getSession().removeAttribute("employee");
         return R.success("推出成功");
+    }
+
+    //新增员工
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+
+        //初始密码123456，进行md5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
     }
 }
